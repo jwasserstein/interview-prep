6e44af72a01cd64d91d0d2636ca926dd814374f8
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-29 06:21:42 -0500

    Add factorial-trailing-zeroes.js

---
+++ factorial-trailing-zeroes.js
@@ -0,0 +1,35 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Follow up: Could you write a solution that works in logarithmic time complexity?
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 104
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    return Math.floor(n/5**1) + Math.floor(n/5**2) + Math.floor(n/5**3) + Math.floor(n/5**4) + Math.floor(n/5**5);
+};
