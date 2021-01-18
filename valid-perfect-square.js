4d5840202d0463accfdf3cd8bb764e9a9e27bd47
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-18 08:24:33 -0500

    Add binary search solution to valid-perfect-square.js

--- valid-perfect-square.js
+++ valid-perfect-square.js
@@ -27,3 +27,28 @@ Constraints:
 var isPerfectSquare = function(num) {
     return (num ** (1/2)) % 1 < 10**(-10)
 };
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare2 = function(num) {
+    if(num === 1 || num === 2147395600) return true;
+    
+    let lower = 1;
+    let upper = num > 46340 ? 46340 : num;
+    let guess = ((upper + lower) / 2) - (((upper + lower) / 2) % 1);
+    
+    while(guess !== lower && guess !== upper) {
+        const square = guess ** 2;
+        if(square === num) return true;
+        if(num > square){
+            lower = guess;
+            guess = ((upper + lower) / 2) - (((upper + lower) / 2) % 1);
+        } else {
+            upper = guess;
+            guess = ((upper + lower) / 2) - (((upper + lower) / 2) % 1);
+        }
+    }
+    return false;
+};
