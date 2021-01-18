f662c94bcf845176c73c357b74372e95f77d1072
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-17 20:37:35 -0500

    Add better solution to container-with-most-water.js

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -46,3 +46,22 @@ var maxArea = function(height) {
     }
     return maxArea;
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1;
+    let maxArea = 0;
+    while(right > left){
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right])*(right-left));
+        if(height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+};
