4ed9749b88d3810e87693886f771e75a4f2b2208
justinwasserstein <justin.wasserstein@gmail.com>
2021-02-01 07:49:54 -0500

    Add bottom-up solution to fibonacci-number.js

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -46,3 +46,15 @@ var fib = function(n) {
     
     return calcFib(n);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib2 = function(n) {
+    const fibNums = [0, 1];
+    for(let i = 2; i <= n; i++){
+        fibNums.push(fibNums[i-1] + fibNums[i-2]);
+    }
+    return fibNums[n];
+};
