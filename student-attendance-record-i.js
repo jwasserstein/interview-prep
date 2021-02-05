a2b22272973e751923faa1bc76e213b2534adf32
justinwasserstein <justin.wasserstein@gmail.com>
2021-02-05 16:09:49 -0500

    Add student-attendance-record-i.js

---
+++ student-attendance-record-i.js
@@ -0,0 +1,40 @@
+/*
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    let aCount = 0;
+    let lCount = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === 'A') {
+            aCount++;
+            if(aCount > 1) return false;
+        }
+        if(s[i] === 'L') {
+            lCount++;
+            if(lCount > 2) return false;
+        } else {
+            lCount = 0;
+        }
+    }
+    
+    return true;
+};
