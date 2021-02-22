ad1376de5fc8f99c09b509bd34c7e3c18ccbffd1
justinwasserstein <justin.wasserstein@gmail.com>
2021-02-22 07:07:35 -0500

    Add maximum-average-subarray-i.js

---
+++ maximum-average-subarray-i.js
@@ -0,0 +1,35 @@
+/*
+Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.
+
+Example 1:
+
+Input: [1,12,-5,-6,50,3], k = 4
+Output: 12.75
+Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+ 
+
+Note:
+
+1 <= k <= n <= 30,000.
+Elements of the given array will be in the range [-10,000, 10,000].
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+    let maxAvg = 0;
+    for(let i = 0; i < k; i++){
+        sum += nums[i];
+    }
+    maxAvg = sum / k;
+    for(let i = k; i < nums.length; i++){
+        sum -= nums[i-k];
+        sum += nums[i];
+        maxAvg = Math.max(maxAvg, sum/k);
+    }
+    return maxAvg;
+};
