a39b51c141fd0cecedf3ea5d39522abb05ba8303
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-15 07:22:50 -0700

    Add to maximum-depth-of-binary-tree.js

--- maximum-depth-of-binary-tree.js
+++ maximum-depth-of-binary-tree.js
@@ -30,3 +30,26 @@ var maxDepth = function(root) {
     }
     return findDepth(root, 1);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    function recursiveDepth(node, depth) {
+        if (!node) {
+            return depth;
+        }
+        return Math.max(recursiveDepth(node.left, depth+1), recursiveDepth(node.right, depth+1));
+    }
+
+    return recursiveDepth(root, 0);
+};
