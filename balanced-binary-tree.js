a1aad30f0abcba17c841d1447710cfe24dfce2a4
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-16 06:09:29 -0700

    Add to balanced-binary-tree.js

--- balanced-binary-tree.js
+++ balanced-binary-tree.js
@@ -28,3 +28,37 @@ var isBalanced = function(root) {
     return (Math.abs(heightOf(root.left) - heightOf(root.right)) <= 1) && 
             isBalanced(root.left) && isBalanced(root.right);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  function recursiveBalanceCheck(node) {
+    if (!node) {
+      return { balanced: true, depth: 0 };
+    } else if (!node.left && !node.right) {
+      return { balanced: true, depth: 1 };
+    }
+
+    const left = recursiveBalanceCheck(node.left);
+    const right = recursiveBalanceCheck(node.right);
+    
+    const depthDifference = Math.abs(left.depth - right.depth);
+    
+    return {
+      balanced: left.balanced && right.balanced && depthDifference <= 1,
+      depth: Math.max(left.depth, right.depth) + 1
+    }
+  }
+  
+  return recursiveBalanceCheck(root).balanced;
+};
