5c46eaaee1a2109992c2d2ee1dedf1a2a822ca35
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-16 06:44:18 -0700

    Add to path-sum.js

--- path-sum.js
+++ path-sum.js
@@ -42,3 +42,28 @@ var hasPathSum = function(root, sum) {
     }
     return checkPath(root, 0);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) {
+      return false;
+    }
+    if (!root.left && !root.right) {
+      return targetSum === root.val;
+    }
+    
+    return hasPathSum(root.left, targetSum - root.val)
+        || hasPathSum(root.right, targetSum - root.val);
+  };
