b390187fda79d5d6d1fc8e61697b72a05b31370c
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-16 06:32:08 -0700

    Add to minimum-depth-of-binary-tree.js

--- minimum-depth-of-binary-tree.js
+++ minimum-depth-of-binary-tree.js
@@ -30,3 +30,31 @@ var minDepth = function(root) {
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (!root) {
+      return 0;
+    } else if (!root.left && !root.right) {
+      return 1;
+    }
+  
+    if (!root.left) {
+      return minDepth(root.right) + 1;
+    } else if (!root.right) {
+      return minDepth(root.left) + 1;
+    } else {
+      return Math.min(minDepth(root.right), minDepth(root.left)) + 1;
+    }
+  };
