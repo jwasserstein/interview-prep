f33cf60779e138c220c6ac1ad605c7b218d57645
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-18 07:15:46 -0700

    Add to contains-duplicate.js

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -31,3 +31,20 @@ var containsDuplicate = function(nums) { //time: O(n), space: O(n)
     }
     return false;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+    
+    for(let i = 0; i < nums.length; i++) {
+      if (seen.has(nums[i])) {
+        return true;
+      }
+      seen.add(nums[i]);
+    }
+    
+    return false;
+};
