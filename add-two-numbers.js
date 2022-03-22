c4f0ec6f5fa2027f199c95e0d5d77122d1c0e924
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-22 06:26:33 -0700

    Add to add-two-numbers.js

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -60,3 +60,40 @@ var addTwoNumbers = function(l1, l2) {
     }
     return root;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let l3;
+    let head;
+    
+    let carry = 0;
+    while (l1 || l2 || carry) {
+      const digitSum = (l1?.val || 0) + (l2?.val || 0) + carry;
+      const val = digitSum % 10;
+      carry = Math.floor(digitSum / 10);
+  
+      if (!l3) {
+        l3 = new ListNode(val, null);
+        head = l3;
+      } else {
+        l3.next = new ListNode(val, null);
+        l3 = l3.next;
+      }
+      l1 = l1?.next;
+      l2 = l2?.next;
+    }
+    
+    return head;
+};
+  
