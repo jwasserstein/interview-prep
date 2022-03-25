cbf9ea1e06194be99c7cbef894524b805d320344
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-25 06:53:52 -0700

    Add to first-bad-version.js

--- first-bad-version.js
+++ first-bad-version.js
@@ -61,3 +61,39 @@ var solution = function(isBadVersion) {
         return version;
     };
 };
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+      let left = 1;
+      let right = n;
+      
+      while (right - left > 1) {
+        const curr = Math.round((left+right)/2);
+        if (isBadVersion(curr)) {
+          right = curr;
+        } else {
+          left = curr;
+        }
+      }
+      
+      return isBadVersion(left) ? left : right;
+    };
+};
