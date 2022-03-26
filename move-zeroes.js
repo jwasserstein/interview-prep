75878feefa9ca04a0d8697556d485c44ce6d2c23
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-26 07:54:50 -0700

    Add to move-zeroes.js

--- move-zeroes.js
+++ move-zeroes.js
@@ -31,3 +31,24 @@ var moveZeroes = function(nums){
     }
     return nums
 }
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let left = 0;
+    
+    while (left < nums.length && nums[left] !== 0) left++;
+    let right = left + 1;
+    
+    while (right < nums.length) {
+      while (nums[right] === 0) {
+        right++;
+        if (right >= nums.length) return;
+      }
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      while (left < nums.length && nums[left] !== 0) left++;
+    }
+};
+  
