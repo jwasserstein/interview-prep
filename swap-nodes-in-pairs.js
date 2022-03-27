56fb20e72bf4db851438c983a2010f078a79e361
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-27 08:21:01 -0700

    Add to swap-nodes-in-pairs.js

--- swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs.js
@@ -59,3 +59,35 @@ var swapPairs = function(head) {
     
     return newHead;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    
+    let prev = new ListNode(null, head);
+    let curr = head;
+    let next = head.next;
+    
+    while (curr && next) {
+      if (curr === head) head = next;
+      curr.next = next.next;
+      next.next = curr;
+      prev.next = next;
+      
+      prev = prev.next.next;
+      curr = prev?.next;
+      next = curr?.next;
+    }
+    
+    return head;
+};
