57d7e559f1298a705b1bdf1395d05e4761658aa5
justinwasserstein <justin.wasserstein@gmail.com>
2022-03-31 05:36:11 -0700

    Add to power-of-four.js

--- power-of-four.js
+++ power-of-four.js
@@ -34,3 +34,23 @@ Follow up: Could you solve it without loops/recursion?
 var isPowerOfFour = function(n) {
     return Math.log(n)/Math.log(4) % 1 < 10**(-10);
 };
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    for (let i = 0; i <= 15; i++) {
+      if (4**i === n) return true;
+    }
+    return false;
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    return (n & (n-1)) === 0 && ((n & 0b1010101010101010101010101010101) !== 0)
+};
+  
