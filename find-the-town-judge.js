174cea5c815f35a41477062123b55ba1849bb133
justinwasserstein <justin.wasserstein@gmail.com>
2022-04-29 06:58:14 -0700

    Add find-the-town-judge.js

---
+++ find-the-town-judge.js
@@ -0,0 +1,70 @@
+/*
+In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+ 
+
+Example 1:
+
+Input: n = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+ 
+
+Constraints:
+
+1 <= n <= 1000
+0 <= trust.length <= 104
+trust[i].length == 2
+All the pairs of trust are unique.
+ai != bi
+1 <= ai, bi <= n
+*/
+
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+  const noTrustSet = createPeopleSet(n);
+  
+  for (let i = 0; i < trust.length; i++) {
+    noTrustSet.delete(trust[i][0]);
+  }
+  if (noTrustSet.size !== 1) return -1;
+  
+  const townJudgeCandidate = Array.from(noTrustSet)[0];
+  const doesntTrustJudge = createPeopleSet(n);
+  doesntTrustJudge.delete(townJudgeCandidate);
+  for (let i = 0; i < trust.length; i++) {
+    if (trust[i][1] === townJudgeCandidate) {
+      doesntTrustJudge.delete(trust[i][0]);
+    }
+  }
+  if (doesntTrustJudge.size > 0) return -1;
+  return townJudgeCandidate;
+};
+
+function createPeopleSet(n) {
+  const people = new Set();
+  for (let i = 1; i <= n; i++) {
+    people.add(i);
+  }
+  return people;
+}
