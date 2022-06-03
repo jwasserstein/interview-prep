3354868ea2bb173a23525a5568423bd03cbb8be2
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-03 07:49:15 -0700

    Add kth-smallest-element-in-a-bst

--- kth-smallest-element-in-a-bst.js
+++ kth-smallest-element-in-a-bst.js
@@ -56,3 +56,36 @@ Follow up: If the BST is modified often (i.e., we can do insert and delete opera
     
     return inOrder(root);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let counter = 1;
+    function recursiveFind(node) {
+      if (node) {
+        if (node.left) {
+          const val = recursiveFind(node.left);
+          if (val !== undefined) return val;
+        }
+        if (counter === k) return node.val;
+        counter++;
+        if (node.right) {
+          const val = recursiveFind(node.right);
+          if (val !== undefined) return val;
+        }
+      }
+    }
+    return recursiveFind(root);
+};
+  
