6017b784f142c9271077b1686c4b7ba56bb4175f
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-04 20:52:46 -0700

    Add to sort-colors.js

--- sort-colors.js
+++ sort-colors.js
@@ -66,3 +66,24 @@ var sortColors = function(nums) {
         else nums[i] = 2;
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    const counts = new Array(3).fill(0);
+    
+    for (let i = 0; i < nums.length; i++) {
+      counts[nums[i]]++;
+    }
+    
+    let k = 0;
+    for (let i = 0; i < counts.length; i++) {
+      for (let j = 0; j < counts[i]; j++) {
+        nums[k] = i;
+        k++;      
+      }
+    }
+};
+  
