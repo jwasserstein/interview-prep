affb55174bbd80c5d6988886022614768caa2279
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-06 06:19:51 -0700

    Add reverse-linked-list.js

--- reverse-linked-list.js
+++ reverse-linked-list.js
@@ -88,3 +88,30 @@ var reverseList = function(head) {
     return prevNode;
 };
   
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    if (!head || !head.next) return head;
+      
+    let prev = null;
+    let curr = head;
+    let next = head.next;
+    
+    while (curr) {
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+      next = next?.next;
+    }
+  
+    return prev;
+};
