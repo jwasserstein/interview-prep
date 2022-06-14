ff3f58707dc77f8b459ff1d26fc895eff7871ed1
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-14 08:29:00 -0700

    Add to 01-matrix.js

---
+++ 01-matrix.js
@@ -0,0 +1,57 @@
+/*
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+Example 2:
+
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+mat[i][j] is either 0 or 1.
+There is at least one 0 in mat.
+*/
+
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+  const out = mat.map(row => row.map(() => Infinity));
+  
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 0) {
+        out[i][j] = 0;
+      } else {
+        out[i][j] = Math.min(out[i][j-1] ?? Infinity, out[i-1]?.[j] ?? Infinity) + 1;
+      }
+    }
+  }
+
+  for (let i = mat.length-1; i >= 0; i--) {
+    for (let j = mat[i].length-1; j >= 0; j--) {
+      out[i][j] = Math.min(
+        out[i][j],
+        Math.min(out[i][j+1] ?? Infinity, out[i+1]?.[j] ?? Infinity) + 1
+      );
+    }
+  }
+
+  return out;
+}
