6a106d288d2d7810a4d7f13ced6a6b52e9f480cf
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-28 07:26:44 -0700

    Add to best-time-to-buy-and-sell-stock.js

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -64,3 +64,21 @@ var maxProfit = function(prices) {
   
   return maxProfit;
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  if (prices.length < 2) return 0;
+
+  let min = prices[0];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    const potentialProfit = prices[i] - min;
+    maxProfit = Math.max(maxProfit, potentialProfit);
+    min = Math.min(min, prices[i]);
+  }
+  
+  return maxProfit;
+};
