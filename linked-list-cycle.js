656055b57245f819a9f6035250e72a5ff041e71e
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-28 18:31:03 -0700

    Add linked-list-cycle.js

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -91,3 +91,28 @@ var hasCycle = function(head) {
     
     return true;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  let slow = head;
+  let fast = head;
+  
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next?.next;
+    if (slow === fast) return true;
+  }
+  
+  return false;
+};
