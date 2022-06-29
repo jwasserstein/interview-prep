3104d079c761b37addd859c02cd865c13fe676c1
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-28 18:35:49 -0700

    Add to implement-stack-using-queues.js

--- implement-stack-using-queues.js
+++ implement-stack-using-queues.js
@@ -157,3 +157,56 @@ MyStack.prototype.empty = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.empty()
  */
+
+
+var MyStack = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    for (let i = 0; i < this.queue.length - 1; i++) {
+        const val = this.queue.shift();
+        this.queue.push(val);
+    }
+    return this.queue.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    for (let i = 0; i < this.queue.length - 1; i++) {
+        const val = this.queue.shift();
+        this.queue.push(val);
+    }
+    const val = this.queue.shift();
+    this.queue.push(val);
+    return val;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
