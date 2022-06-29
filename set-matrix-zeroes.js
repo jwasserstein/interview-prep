4667ead214bb74ff3488667e90cc56cb617ecf68
justinwasserstein <justin.wasserstein@gmail.com>
2022-06-28 18:11:45 -0700

    Add to set-matrix-zeroes.js

--- set-matrix-zeroes.js
+++ set-matrix-zeroes.js
@@ -132,3 +132,48 @@ n == matrix[0].length
         }
     }
 };
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let zeroFirstRow = matrix[0].includes(0);
+    let zeroFirstCol = matrix.map(row => row[0]).includes(0);
+  
+    for (let row = 1; row < matrix.length; row++) {
+      for (let col = 1; col < matrix[row].length; col++) {
+        if (matrix[row][col] === 0) {
+          matrix[0][col] = 0;
+          matrix[row][0] = 0;
+        }
+      }
+    }
+    
+    for (let row = 1; row < matrix.length; row++) {
+      if (matrix[row][0] === 0) {
+        for (let col = 1; col < matrix[row].length; col++) {
+          matrix[row][col] = 0;
+        }
+      }
+    }
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[0][col] === 0) {
+        for (let row = 1; row < matrix.length; row++) {
+          matrix[row][col] = 0;
+        }
+      }
+    }
+  
+    if (zeroFirstCol) {
+      for (let row = 0; row < matrix.length; row++) {
+        matrix[row][0] = 0;
+      }
+    }
+    if (zeroFirstRow) {
+      for (let col = 0; col < matrix[0].length; col++) {
+        matrix[0][col] = 0;
+      }
+    }
+};
+  
