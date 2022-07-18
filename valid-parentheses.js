6c61e4b0cc43219f34fd414715dd00d6ad4e3d59
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-07-17 19:08:14 -0700

    Add to valid-parentheses.js

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -111,3 +111,29 @@ var isValid = function(s) {
   
   return stack.length === 0;
 };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const parens = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  const stack = [];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] in parens) {
+      stack.push(s[i]);
+    } else {
+      if (parens[stack[stack.length-1]] !== s[i]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  
+  return stack.length === 0;
+};
