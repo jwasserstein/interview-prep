6b5d78722f53261f64ca1d75e33f796de1d15117
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-07-31 19:34:16 -0700

    Add to lowest-common-ancestor-of-a-binary-search-tree.js

--- lowest-common-ancestor-of-a-binary-search-tree.js
+++ lowest-common-ancestor-of-a-binary-search-tree.js
@@ -146,3 +146,26 @@ var lowestCommonAncestor = function(root, p, q) {
   }
 };
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (q.val < root.val && p.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (q.val > root.val && p.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
