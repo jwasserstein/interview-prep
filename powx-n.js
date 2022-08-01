52e38f4744d23c8727fc0f4a92965815a1770d56
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-01 09:14:14 -0700

    Add to powx-n.js

--- powx-n.js
+++ powx-n.js
@@ -46,3 +46,20 @@ var myPow = function(x, n) {
   }
 };
 
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if (n === 0) return 1;
+  if (n === 1) return x;
+  if (n < 0) return 1/myPow(x, n*-1);
+  
+  if (n % 2 === 0) {
+    const pow = myPow(x, n/2);
+    return pow*pow;
+  } else {
+    return myPow(x, n-1) * x;
+  }
+};
