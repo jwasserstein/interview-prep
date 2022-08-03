8dc7430f5ba601054d2adfd4f4bc9b76b09ced76
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-03 10:38:11 -0700

    Add to binary-tree-postorder-traversal.js

--- binary-tree-postorder-traversal.js
+++ binary-tree-postorder-traversal.js
@@ -56,3 +56,28 @@ var postorderTraversal = function(root) {
 
   return out;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  const out = [];
+  function dfs(node) {
+    if (!node) return;
+    
+    dfs(node.left);
+    dfs(node.right);
+    out.push(node.val);
+  }
+  dfs(root);
+  return out;
+};
