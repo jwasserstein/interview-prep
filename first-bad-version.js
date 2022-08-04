c2bc368059fda9947914b60e98a30f99272bc1d1
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-04 07:07:53 -0700

    Add to first-bad-version.js

--- first-bad-version.js
+++ first-bad-version.js
@@ -137,3 +137,41 @@ var solution = function(isBadVersion) {
     return right;
   };
 };
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+      if (isBadVersion(1)) return 1;
+      
+      let left = 2;
+      let right = n;
+      while (left <= right) {
+        const guess = Math.floor((left + right) / 2);
+        
+        if (isBadVersion(guess) && isBadVersion(guess-1)) {
+          right = guess - 1;
+        } else if (!isBadVersion(guess) && !isBadVersion(guess-1)) {
+          left = guess + 1;
+        } else {
+          return guess;
+        }
+      }
+    };
+};
