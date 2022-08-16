02d3fc09650b5b8135a66e75b5935953c7120e7a
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-15 20:25:29 -0700

    Add to kth-smallest-element-in-a-bst.js

--- kth-smallest-element-in-a-bst.js
+++ kth-smallest-element-in-a-bst.js
@@ -89,3 +89,32 @@ var kthSmallest = function(root, k) {
     return recursiveFind(root);
 };
   
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+  let count = 0;
+  let kthSmallestValue = null;
+  function dfs(node) {
+    if (!node || kthSmallestValue !== null) return;
+    
+    dfs(node.left);
+    count++;
+    if (count === k) {
+      kthSmallestValue = node.val;
+    }
+    dfs(node.right);
+  }
+  dfs(root);
+  return kthSmallestValue;
+};
