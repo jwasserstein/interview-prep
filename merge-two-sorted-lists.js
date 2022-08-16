315805bbdb42d19f8a0a964a086c2b798eefd88e
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-16 08:43:21 -0700

    Add to merge-two-sorted-lists.js

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -71,3 +71,43 @@ Both list1 and list2 are sorted in non-decreasing order.
   
   return head;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  if (!list1 || !list2) return list1 || list2;
+  
+  let head;
+  if (list1.val < list2.val) {
+    head = list1;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    list2 = list2.next;
+  }
+  let curr = head;
+  
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = list1 || list2;
+  
+  return head;
+};
