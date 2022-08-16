f0f4661a2a3dae5412ab247b6b37d94e8cb6ab1b
Justin Wasserstein <65873487+jwasserstein@users.noreply.github.com>
2022-08-15 20:29:43 -0700

    Add to binary-tree-level-order-traversal.js

--- binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal.js
@@ -81,3 +81,29 @@ var levelOrder = function(root) {
   dfs(root, 0);
   return out;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  const out = [];
+  function dfs(node, level) {
+    if (!node) return;
+    if (!out[level]) out[level] = [];
+    
+    dfs(node.left, level+1);
+    out[level].push(node.val);
+    dfs(node.right, level+1);
+  }
+  dfs(root, 0);
+  return out;
+};
